3ff4c9db478137916f7eabe64a86c9e939d93098
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 21:44:39 +0700

    add administrator as default admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Administrator.create(email: "admin@email.com", password: "secret")
