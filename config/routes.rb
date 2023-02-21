cf7a1f515d7ff6a0309c09a0e77e9eb3233ce05f
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 13:24:09 +0700

    add sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  unless Rails.env.development?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      username == Rails.application.credentials(:sidekiq, :username) && password == Rails.application.credentials(:sidekiq, :password)
+    end
+  end
+
+  mount Sidekiq::Web => '/sidekiq'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
