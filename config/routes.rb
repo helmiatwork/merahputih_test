dd8d1ec8cfa383cab6bcc68ecb7dd23f494616b7
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 20:33:48 +0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root to: "admin/users#index"
   namespace :admin do
+    root to: redirect("admin/users")
     resources :users
     resources :transactions
   end
