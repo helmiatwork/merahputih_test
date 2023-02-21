253e84729e6872b9a573025424d90626aa59953d
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 21:42:08 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_060731) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_134544) do
+  create_table "administrators", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_administrators_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
+  end
+
   create_table "double_entry_account_balances", force: :cascade do |t|
     t.string "account", null: false
     t.string "scope"
