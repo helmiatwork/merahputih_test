c06703934ade1a7f7944277727547410fea3e7c0
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 20:49:45 +0700

    generate devise administrator

---
+++ app/models/administrator.rb
@@ -0,0 +1,9 @@
+class Administrator < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  valudates :password, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :administrators
+  
   unless Rails.env.development?
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
       username == Rails.application.credentials(:sidekiq, :username) && password == Rails.application.credentials(:sidekiq, :password)
