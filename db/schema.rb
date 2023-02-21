f3ba5be0b3ba764b2ef41d57fb4f6e289735e31c
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 14:15:43 +0700

    add name

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_060731) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
