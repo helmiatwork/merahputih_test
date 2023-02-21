87e2d6f97ce5b01c8b283076ae72a984789bff9d
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 20:49:17 +0700

    validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  valudates :password, presence: true
 end
