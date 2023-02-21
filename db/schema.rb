207b5b8d8c2b8ae0fb286b8899bbec0b551f90a1
helmiatwork <helmi.at.work@gmail.com>
2023-02-21 13:12:56 +0700

    update schema

---
+++ db/schema.rb
@@ -0,0 +1,65 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_060731) do
+  create_table "double_entry_account_balances", force: :cascade do |t|
+    t.string "account", null: false
+    t.string "scope"
+    t.bigint "balance", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account"], name: "index_account_balances_on_account"
+    t.index ["scope", "account"], name: "index_account_balances_on_scope_and_account", unique: true
+  end
+
+  create_table "double_entry_line_checks", force: :cascade do |t|
+    t.integer "last_line_id", null: false
+    t.boolean "errors_found", null: false
+    t.text "log"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at", "last_line_id"], name: "line_checks_created_at_last_line_id_idx"
+  end
+
+  create_table "double_entry_lines", force: :cascade do |t|
+    t.string "account", null: false
+    t.string "scope"
+    t.string "code", null: false
+    t.bigint "amount", null: false
+    t.bigint "balance", null: false
+    t.integer "partner_id"
+    t.string "partner_account", null: false
+    t.string "partner_scope"
+    t.string "detail_type"
+    t.integer "detail_id"
+    t.json "metadata"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account", "code", "created_at"], name: "lines_account_code_created_at_idx"
+    t.index ["account", "created_at"], name: "lines_account_created_at_idx"
+    t.index ["scope", "account", "created_at"], name: "lines_scope_account_created_at_idx"
+    t.index ["scope", "account", "id"], name: "lines_scope_account_id_idx"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
