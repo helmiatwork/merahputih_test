82f68075b667af6ed0f37e2e384104dda1a6dc02
helmiatwork <helmi.at.work@gmail.com>
2023-02-22 10:06:08 +0700

    add model test

--- test/models/administrator_test.rb
+++ test/models/administrator_test.rb
@@ -1,7 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class AdministratorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save administrator without email" do
+    administrator = Administrator.new(password: "password")
+    assert_not administrator.save, "Saved the administrator without an email"
+  end
+
+  test "should not save administrator without password" do
+    administrator = Administrator.new(email: "admin@example.com")
+    assert_not administrator.save, "Saved the administrator without a password"
+  end
+
+  test "should save administrator with email and password" do
+    administrator = Administrator.new(email: "admin@example.com", password: "password")
+    assert administrator.save, "Could not save the administrator with email and password"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  
+  test "should not save user without name" do
+    user = users(:one)
+    user.name = nil
+    assert_not user.save
+  end
+
+  test "should not save user without email" do
+    user = users(:one)
+    user.email = nil
+    assert_not user.save
+  end
+
+  test "should not save user with invalid email" do
+    user = users(:one)
+    user.email = "invalid_email"
+    assert_not user.save
+  end
+
+  test "should not save user without password" do
+    user = users(:one)
+    user.password = nil
+    assert_not user.save
+  end
 end
