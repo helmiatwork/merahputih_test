8f65d74721025865f01f8f7b15711302d316bada
helmiatwork <helmi.at.work@gmail.com>
2023-02-22 08:42:57 +0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Administrator.create(email: "admin@email.com", password: "secret")
+
+User.create(name: "helmi", email: "helmi@gmail.com", password: "secret")
+User.create(name: "adnreas", email: "andreas@gmail.com", password: "secret")
+
+#Deposit
+10.times do
+    TransferService.execute(to_id: User.first.id, transfer_amount: rand(10) * 1000, transfer_code: "deposit")
+    TransferService.execute(to_id: User.second.id, transfer_amount: rand(10) * 1000, transfer_code: "deposit")
+end
+
+#withdraw
+5.times do
+    TransferService.execute(from_id: User.first.id, transfer_amount: rand(10) * 100, transfer_code: "withdraw")
+    TransferService.execute(from_id: User.second.id, transfer_amount: rand(10) * 100, transfer_code: "withdraw")
+end
+
+#transfer
+5.times do
+    TransferService.execute(from_id: User.second.id, to_id: User.first.id, transfer_amount: rand(10) * 100, transfer_code: "transfer")
+    TransferService.execute(from_id: User.first.id, to_id: User.second.id, transfer_amount: rand(10) * 100, transfer_code: "transfer")
+end
